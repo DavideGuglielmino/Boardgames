1171e42043e1c2bd16bfb42e5cd91f3051eb11e1
DavideGuglielmino <davideguglielmino.programmer@gmail.com>
2020-09-03 17:15:27 +0200

    Singleton, GameManager and SceneLoader

---
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : Singleton<SceneLoader>
+{
+    public void LoadScene(int scene)
+    {
+        AsyncOperation a = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+    }
+
+    public void LoadScene(string scene)
+    {
+        AsyncOperation a = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+    }
+}
